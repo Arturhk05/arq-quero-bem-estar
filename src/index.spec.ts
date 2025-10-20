af751e8653004eab5aedb3fcfd3364decc192674
Arturhk05 <arturhandow@gmail.com>
2025-10-20 09:21:31 -0300

    chore: add express

--- src/index.spec.ts
+++
@@ -1,6 +0,0 @@
-import Person from "."
-
-it("should do something", () => {
-  const person = new Person()
-  expect(person.sayMyName()).toBe("My name is John Doe")
-})
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-class Person {
-  sayMyName(): string {
-    return "My name is John Doe"
-  }
-}
-
-export default Person
