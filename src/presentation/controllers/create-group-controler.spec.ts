d596f1ca450f36ca5e33bb6c9e2a678fefb255dd
Arturhk05 <arturhandow@gmail.com>
2025-10-20 08:53:57 -0300

    test: ensure CreateGroupController returns 400 if no name is provided

--- src/presentation/controllers/create-group-controler.spec.ts
+++ src/presentation/controllers/create-group-controler.spec.ts
@@ -1,11 +1,23 @@
 import { CreateGroupController } from "./create-group-controller"
 
 describe("CreateGroupController", () => {
-  it("should return 400 Not Implemented if userId is not provided", () => {
+  it("should return 400 if userId is not provided", () => {
     const sut = new CreateGroupController()
     const httpRequest = {
       body: {
         name: "any_name",
+        duracaoDias: 30,
+      },
+    }
+    const response = sut.handle(httpRequest)
+    expect(response.status).toBe(400)
+  })
+
+  it("should return 400 if name is not provided", () => {
+    const sut = new CreateGroupController()
+    const httpRequest = {
+      body: {
+        userId: "any_user_id",
         duracaoDias: 30,
       },
     }
