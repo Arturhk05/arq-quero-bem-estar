82796b4ae2925213132cf52ca5f308fee4e0eadf
Arturhk05 <arturhandow@gmail.com>
2025-10-21 15:26:36 -0300

    feat: add typeorm inicialization

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,9 +1,18 @@
 import "dotenv/config"
+import "reflect-metadata"
 import express from "express"
 import env from "./config/env"
+import { TypeOrmConnection } from "@/infra/database/typeorm/typeorm-connection"
 
 const app = express()
 
-app.listen(env.port, () => {
-  console.log(`Server is running on http://localhost:${env.port}`)
-})
+TypeOrmConnection.getInstance()
+  .initialize()
+  .then(() => {
+    app.listen(env.port, () => {
+      console.log(`Server is running on http://localhost:${env.port}`)
+    })
+  })
+  .catch((error) => {
+    console.error("Error during Data Source initialization:", error)
+  })
